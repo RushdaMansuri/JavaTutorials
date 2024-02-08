58bb405ed0d3cc1c49e160ab66561947c0bcafab
rmansuri <mansurirushda7@gmail.com>
2024-02-08 17:00:14 -0500

    Recusion

--- ForEachLoop/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("ForEach Loop:");
-        int []a = {1,2,3,4,5,6,7,8,9,0};
-        for(int element: a){
-            System.out.println(element);
-        }
-    }
-}
