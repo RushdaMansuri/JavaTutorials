8d267ece180f271d1537743579555bb8826c36bd
rmansuri <mansurirushda7@gmail.com>
2024-02-08 19:02:31 -0500

    Added

--- Recursion/src/Main.java
+++ Recursion/src/Main.java
@@ -8,11 +8,25 @@ public class Main {
         else
             return n * factorial(n-1);
     }
+
+    static long fibonacci(int n){
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Recursion.");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number to find it's Factorial: ");
+        System.out.println("Enter a number to find it's Factorial and Fibonacci: ");
         int num = sc.nextInt();
         System.out.println("Factorial of " + num + ": " + factorial(num));
+
+        //fibonacci
+        System.out.println("Fibonacci of " + num + ": ");
+        for(int i = 0; i < num; i++){
+            System.out.print(fibonacci(i) + " ");
+        }
     }
 }
