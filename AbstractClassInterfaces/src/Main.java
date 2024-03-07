676e0ae83bef0344028118fee64c113638515bfd
rmansuri <mansurirushda7@gmail.com>
2024-03-06 22:04:09 -0500

    Added AbstractClassInterface

---
+++ AbstractClassInterfaces/src/Main.java
@@ -0,0 +1,25 @@
+abstract class Base{
+    public Base(){
+        System.out.println("I am Base class Constructor");
+    }
+
+    public void sayHello(){
+        System.out.println("Hello from Base.");
+    }
+
+    abstract public void greet();
+}
+
+class Derived extends Base{
+    @Override
+    public void greet(){
+        System.out.println("Hello from Derived.");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Abstract Class & Interface.");
+        Derived d = new Derived();
+        d.greet();
+    }
+}
